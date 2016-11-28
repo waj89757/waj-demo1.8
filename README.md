8c1e5606d519b39298df13dab057538753e52d01
wanganjie <wanganjie@xiaomi.com>
2016-11-28 11:11:19 +0800

    add akka-demo

---
+++ README.md
@@ -0,0 +1,2 @@
+1.summing up the character of jdk1.8
+2.akka program
--- jav1.8/src/main/MainTest.java
+++ jav1.8/src/main/MainTest.java
@@ -2,6 +2,12 @@ package main;
 
 /**
  * Created by waj on 16-11-28.
+ * first test in jdk1.8
  */
 public class MainTest {
+
+    public static void main(String args[]){
+
+    }
+
 }
